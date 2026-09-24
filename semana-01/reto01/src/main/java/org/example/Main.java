a3a60e3b232a3ad312fb797f4b07f3514a245a74
davidfg02 <davidfrgarcia@gmail.com>
2026-09-24 20:01:28 +0200

    feat: add Plataforma y corregido Main

--- semana-01/reto01/src/main/java/org/example/Main.java
+++ semana-01/reto01/src/main/java/org/example/Main.java
@@ -6,20 +6,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("--- INICIANDO UDITVERSUM ---");
         //MALA PRÁCTICA: Toda la lógica de negocio tirada al Main, es decir, los diamantes <> obligan a que la lista solo acepte episodios
-        ArrayList<Episodio> catalogo = new ArrayList<>();
-        catalogo.add(new Episodio("Diseño 3D - Intro", 45));
-        catalogo.add(new Episodio("Animación", 60));
-        catalogo.add(new Episodio("Texturas", 50));
+        Plataforma plataforma = new Plataforma();
+        plataforma.agregarEpisodio(new Episodio("Diseño 3D", 46));
+        plataforma.agregarEpisodio(new Episodio("Animacion", 60));
+        plataforma.agregarEpisodio(new Episodio("Programacion", 82));
+        plataforma.agregarEpisodio(new Episodio("ASIR", 64));
+        plataforma.agregarEpisodio(new Episodio("Diseño 3D - Fin", 46));
 
         // 2. MEDICION DE TIEMPO
         //Usamos long porque los ms son desde 1970 forman un numero tan gigantesco, que no cabe en la memoria int
         long inicio = System.currentTimeMillis();
         //El bucle FOR-EACH "Por cada episodio dentro del catalogo"
-        for (Episodio ep : catalogo) {
-            ep.procesar();//Aquí ocurre el bloqueo de 2s
-        }
+        plataforma.procesarCatalogo();
         long fin = System.currentTimeMillis();
-
         //RESULTADO: Restamos fin menos inicioy dividimos entre 1000 para que salga en (s)
         long totalSegundo = (fin - inicio) / 1000;
         System.out.println("Tiempo total = " + totalSegundo + " segundos de bloqueo");
---
+++ semana-01/reto01/src/main/java/org/example/Plataforma.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Plataforma {
+    private ArrayList<Episodio> catalogo;
+
+    public Plataforma(){
+        this.catalogo = new ArrayList<>();
+    }
+
+    public void agregarEpisodio(Episodio e){
+        this.catalogo.add(e);
+    }
+    public void procesarCatalogo(){
+        for (Episodio episodio : this.catalogo){
+            episodio.procesar();
+        }
+    }
+}
